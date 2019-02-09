947633c2c0959b92402b434f50af63ee1e0d31f1
classy-debugger-3131 <elle.valentine@hotmail.com>
2019-02-09 05:57:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var playlist = { "My Bloody Valentine":"Soon", "Phil Ochs":"Changes", "Slowdive"
 
 function updatePlaylist(playlist, artistName, songTitle){ playlist[artistName]=songTitle; return playlist}  
 
-function removeFromPlaylist(playlist, artistName){  delete playlist[artistName]} 
+function removeFromPlaylist(playlist, artistName){  delete playlist[artistName]; return playlist} 
